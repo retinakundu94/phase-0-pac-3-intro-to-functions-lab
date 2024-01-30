15d03ee8ccd85f39398e478aeec7c535ea4b3305
Retina Kundu <retinakundu94@gmail.com>
2024-01-30 18:28:07 -0500

    complete lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,8 @@
 require ( './root.js' );
 
 
-describe('shout(string)', function() {
+describe('shout(string)', function () {
+  
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO');
   })
